82e6330917d53331fdda4fe28a4fdcb2c3f37eea
EDITH311 <102552422+EDITH311@users.noreply.github.com>
2022-04-06 17:00:30 +0300

    Add files via upload

---
+++ program_to_determine_whether_a_number_is_divisible_by_5or_not[1].c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int num;
+printf("the num:");  
+scanf("%d",&num);
+//it is true if the number is completely divisible by 5
+if(num%=0)
+printf("%d is divisible.",num);
+else
+printf("%d is not divisible.",num);
+printf("to determine whether a number is divible by 5or not");
+return 0;
+}
---
+++ programming_to_the_sum_of_two_numbers[1].c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a;
+int b;
+int sum;
+printf("enter the first number\n");
+scanf("%d",&a);
+printf("enter the second number\n");
+scanf("%d",&b);
+sum=a+b;
+ printf("the sum of two numbers:%d",sum);   
+ return 0 ;
+}
